6c942a8bda56f8475bb80e66ef22de6322cf206e
Saad_Hodekar <183364486+Magneto008@users.noreply.github.com>
2026-04-01 22:25:41 +0530

    Initial commit

---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+export type Transaction = {
+  id: string;
+  date: string;
+  amount: number;
+  category: string;
+  type: "income" | "expense";
+};
+
+export type Role = "viewer" | "admin";
+
+export type FilterField = "amount" | "category" | "type" | "date";
+export type FilterOperator = "=" | ">" | "<" | "contains";
+export type LogicalOperator = "AND" | "OR";
+
+export type FilterCondition = {
+  id: string;
+  field: FilterField;
+  operator: FilterOperator;
+  value: string;
+};
+
+export type FilterGroup = {
+  id: string;
+  conditionJoin: LogicalOperator;
+  conditions: FilterCondition[]; // Conditions combined by selected join operator
+};
+
+export type AppState = {
+  transactions: Transaction[];
+  role: Role;
+  filterGroups: FilterGroup[]; // Groups combined by OR
+};
